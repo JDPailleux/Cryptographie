7e3d767d1c77463645a28060e9644f962782f512
Pailleux <jean-di.pailleux@outlook.com>
2017-04-17 12:17:24 +0200

    Test fermat + square_multiply finis

--- DM.c
+++ DM.c
@@ -2,18 +2,15 @@
 
 
 void square_multiply(mpz_t r, mpz_t a, mpz_t n, mpz_t h){ // r : resultat de la fonction, n : modulo, h : exposant 
-	mpz_t zero, un, deux, modu;	// "modu" va contenir le reste de la division euclidienne de h par 2 à chaque itération.	
-	mpz_init_set_str(zero,"0",10); // zero = 0 
-	mpz_init_set_str(un,"1",10); // un = 1
-	mpz_init_set_str(deux,"2",10); // deux = 2 
-	mpz_add(r, zero ,a);	    // r = 0 + a => r = a
-
-	while (mpz_cmp(h, zero) > 0){
+	mpz_t modu;	// "modu" va contenir le reste de la division euclidienne de h par 2 à chaque itération.	
+	mpz_init(modu);
+	mpz_set(r,a);
+	while (mpz_cmp_ui(h, 0) > 0){
 		
 		mpz_mul(r, r, r); // r = r*r
 		mpz_mod(r, r, n); // r = r*r modulo n
-		mpz_fdiv_qr(h, modu, h, deux); // modu = reste de la div et h = h/2 pour la prochaine itération
-		if ( mpz_cmp(modu,un) == 0){ // On test h%2 == 1
+		mpz_fdiv_qr_ui(h, modu, h, 2); // modu = reste de la div et h = h/2 pour la prochaine itération
+		if ( mpz_cmp_ui(modu,1) == 0){ // On test h%2 == 1
 			mpz_mul(r, r, a); // r = r*a
 			mpz_mod(r, r, n); // r = r*a modulo n
 		}
@@ -22,30 +19,67 @@ void square_multiply(mpz_t r, mpz_t a, mpz_t n, mpz_t h){ // r : resultat de la 
 
 void testFermat(mpz_t n, int k){	// n: entier et k nombre de répétition
 	int i, res =1;
-	mpz_t val, a, un, n_1;
-	mpz_init_set_str(un,"1",10);   // un = 1 
-	mpz_init(a); 
-	mpz_init(val); 
+	mpz_t val, a, n_1, n_3; //n_1 : correspond à n-1, n_3 : correspond à n-3
+	mpz_init(a);   // Correspond à la valeur aléatoire
+	mpz_init(val); // On stock la valeur de retour de suare_multiply ici
 	mpz_init(n_1);
-	mpz_sub(n_1, n, un);	//n_1 = n-1	
+	mpz_init(n_3);
+	mpz_sub_ui(n_1, n, 1);	//n_1 = n-1	
+	mpz_sub_ui(n_3, n, 3);	//n_1 = n-3
 
 	// Valeur aléatoire 
 	gmp_randstate_t state;
 	gmp_randinit_mt(state);
 	gmp_randseed_ui(state, time(NULL));
 
-	for (i= 1; i <= k; i++){
-		mpz_urandomm(a, state, n); 
-		square_multiply(val, a, n, n_1);
-		if ( mpz_cmp(val, un) == 0) res = 0; 	// n est composé
+	for (i= 1; i <= k; i++){ 
+		mpz_urandomm(a, state, n_3); // On a : 0 <= a <= n-4
+		mpz_add_ui(a,a,2); // a = a+2. On doit alors avoir 1 < a < n-1
+		square_multiply(val, a, n, n_1); 
+		
+		if ( mpz_cmp_ui(val, 1) == 0) res = 0; 	// n est composé
 	}
 	
 	if ( res == 1) gmp_printf("Le nombre %Zd est supposé premier \n",n);
 	else  gmp_printf("Le nombre %Zd est composé \n \n",n);
 }
+/*
+void decompo(mpz_t t, mpz_t s, mpz_t n){ // Fonction qui décompose n-1. (n vaudra n-1)
+	int boolp = 0; // Entier qui va nous permettre de sortir des deux boucles.	
+	mpz_t tmp;
+	mpz_init(tmp);
 
-/*void testMillerRabin(mpz_t n, int k){
+	// On initialise t et s.
+	mpz_init(s);
+	mpz_init(t);
+	// Deux boucles imbriquées qui vont trouver t et s tel que 2^s * t = n-1
+	for ( mpz_sqrt(s, n); mpz_cmp_ui(s,1) >= 0; mpz_sub_ui(s,s,1)){
+		mpz_set_ui(t,1); // t = 1
+		
+		while (1){
 
+		}
+		mpz_add_ui(t,t,2); // t += 2 (car t doit être impair)
+	}
+}
+
+void testMillerRabin(mpz_t n, int k){
+
+	if ( mpz_cmp(n,2) == 0) gmp_printf("premier\n");
+	else if ( mpz_mod(n,2) == 0 ) gmp_printf("composé\n"); // cas nombre paire != 2
+	else {
+
+	int booleen =1;
+	mpz_t t, i, a, y, j, s;
+
+	for (i= 1; i <= k; i++){
+		mpz_urandomm(a, state, n); 
+		square_multiply(val, a, n, n_1);
+		if ( mpz_cmp(val, un) == 0) booleen = 0; 	// n est composé
+	}
+	
+	if ( booleen == 1) gmp_printf("Le nombre %Zd est supposé premier \n",n);
+	else  gmp_printf("Le nombre %Zd est composé \n \n",n);
 
 }*/
 
@@ -56,15 +90,14 @@ int main(){ //int argc, char** argv
 	// Test de la fonction de Fermat:
 	printf("#### TEST DE FERMAT #### \n n = 11, rep = 4\n");
 	
-	testFermat(n,4);
+	testFermat(n,15);
 
-	// Test de la bibliothèque GMP:
+	/*// Test de la bibliothèque GMP:
 	mpz_t rop, base, exp, mod;
 	mpz_init(rop);
 	mpz_init_set_str(base,"2",10);
 	mpz_init_set_str(exp, "5", 10);
 	mpz_init_set_str(mod, "15",10);
-	square_multiply(rop, base, mod, exp);
-	gmp_printf(" %Zd ^ %Zd mod %Zd = %Zd\n",base,exp, mod, rop);
+	square_multiply(rop, base, mod, exp); */
 	return 0;
 }
