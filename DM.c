d9a987cb386a089396f296dc20efc0ee09767b70
Pailleux <jean-di.pailleux@outlook.com>
2017-04-20 21:43:11 +0200

    Mise à jour

--- DM.c
+++ DM.c
@@ -1,48 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <gmp.h>
+#include "DM.h"
 
-void square_multiply(mpz_t r, mpz_t a, mpz_t n, mpz_t h){ // r : resultat de la fonction, n : modulo, h : exposant 
-	mpz_t modu;	// "modu" va contenir le reste de la division euclidienne de h par 2 à chaque itération.	
-	mpz_init(modu);
-	mpz_set(r,a);
-	while (mpz_cmp_ui(h, 0) > 0){
-		
-		mpz_mul(r, r, r); // r = r*r
-		mpz_mod(r, r, n); // r = r*r modulo n
-		mpz_fdiv_qr_ui(h, modu, h, 2); // modu = reste de la div et h = h/2 pour la prochaine itération
-		if ( mpz_cmp_ui(modu,1) == 0){ // On test h%2 == 1
-			mpz_mul(r, r, a); // r = r*a
-			mpz_mod(r, r, n); // r = r*a modulo n
-		}
-	}
-}
+void square_multiply(mpz_t r, mpz_t a, mpz_t n, mpz_t h);
 
-void testFermat(mpz_t n, mpz_t k){	// n: entier et k nombre de répétition
+int testFermat(mpz_t n, mpz_t k){	// n: entier et k nombre de répétition
 	int res =1;
 	mpz_t i, val, a, n_1, n_3; 		 // n_1 : correspond à n-1, n_3 : correspond à n-3,
-	mpz_inits(a, val, n_1, n_3, (void*)0);   // a: Correspond à la valeur aléatoire, val : stock la valeur de retour de suare_multiply
+	mpz_inits(i, a, val, n_1, n_3, (void*)0);// a: Correspond à la valeur aléatoire, val : stock la valeur de retour de suare_multiply
 	
 	mpz_sub_ui(n_1, n, 1);	//n_1 = n-1	
-	mpz_sub_ui(n_3, n, 3);	//n_1 = n-3
+	mpz_sub_ui(n_3, n, 3);	//n_3 = n-3
 
 	// Valeur aléatoire 
 	gmp_randstate_t state;
 	gmp_randinit_mt(state);
 	gmp_randseed_ui(state, time(NULL));
-
+	
 	for (mpz_set_ui(i,1); mpz_cmp(i,k) <= 0; mpz_add_ui(i, i, 1)){ //for (i= 1; i <= k; i++)
-		mpz_urandomm(a, state, n_3); // On a : 0 <= a <= n-4
+		mpz_urandomm(a, state, n_3); // On a : 0 <= a <= n-4 
 		mpz_add_ui(a,a,2); // a = a+2. On doit alors avoir 1 < a < n-1
 		square_multiply(val, a, n, n_1); 
-		gmp_printf("alea : %Zd, val : %Zd\n",a,val);
-		if ( mpz_cmp_ui(val, 1) == 0) res = 0; 	// n est composé
+		mpz_sub_ui(n_1, n, 1);	//n_1 = n-1 car il est mis à zéro dans la fonction précédente
+
+		if ( mpz_cmp_ui(val, 1) != 0) res = 0; 	// n est composé
 	}
 	
-	if ( res == 1) gmp_printf("Le nombre %Zd est premier \n",n);
-	else  gmp_printf("Le nombre %Zd est composé \n \n",n);
-	//mpz_clears(i, val, a, n_1, n_3);
+	if ( res == 1) return 1;
+	else  return 0;	
+	
+	// Libération mémoire
+	mpz_clear(i); mpz_clear(a); mpz_clear(val); mpz_clear(n_3); mpz_clear(n_1); gmp_randclear(state);
 }
 
 void my_pow_2(mpz_t dest, mpz_t exp){ // calcul 2^exp et stock le résultat dans dest.
@@ -78,17 +64,17 @@ void decompo(mpz_t t, mpz_t s, mpz_t n){ // Fonction qui décompose n-1. (n vaud
 	}
 }
 
-void testMillerRabin(mpz_t n, mpz_t k){ // n : nombre à tester, k : nombre de répétition
+int testMillerRabin(mpz_t n, mpz_t k){ // n : nombre à tester, k : nombre de répétition  (renvoie 1 : premier, renvoie 0 : composé)
 
-	if ( mpz_cmp_ui(n,2) == 0) gmp_printf("premier\n");
-	//else if ( mpz_mod_ui(n,2) == 0 ) gmp_printf("composé\n"); // cas nombre paire != 2
+	if ( mpz_cmp_ui(n,2) == 0) return 1;
 	else {
-
-	mpz_t t, i, a, y, j, s, n_1; // n_1 => n-1
-	mpz_inits(t, i, a, y, j, s, (void*) 0);
+	int next;
+	mpz_t tmp_t, t, i, a, y, j, s, n_1; // n_1 => n-1
+	// tmp_t sera utilisé à la place de t dans square_multiply car la fonction va le mettre à zéro (donc perte de la valeur de t)
+	mpz_inits(tmp_t, t, i, a, y, j, s, n_1,(void*) 0); 
 	mpz_sub_ui(n_1,n,1);
 	decompo(t,s,n_1); // On décompose n-1 + initilisation des valeurs de t et s
-	gmp_printf("t: %Zd, s: %Zd\n",t,s);
+	mpz_set(tmp_t,t);
 
 	// Valeur aléatoire 
 	gmp_randstate_t state;
@@ -96,48 +82,106 @@ void testMillerRabin(mpz_t n, mpz_t k){ // n : nombre à tester, k : nombre de r
 	gmp_randseed_ui(state, time(NULL));
 
 	for (mpz_set_ui(i,1); mpz_cmp(i,k) <= 0; mpz_add_ui(i, i, 1)){ //for (i= 1; i <= k; i++)
-
+		next = 0;
 		mpz_urandomm(a, state, n_1);  // on tire a entre 0 et n-2
 		mpz_add_ui(a,a,1); // On a : 0 < a < n
-		square_multiply(y, a, n, t);
-		if ( (mpz_cmp_ui(y, 1) != 1) && (mpz_cmp_ui(y, 1) != -1)){
-			gmp_printf("Le nombre %Zd est composé \n \n",n); 
-			//mpz_clears(t, i, a, y, j, s, n_1); // Libération mémoire
-			return; // On arrête la fonction.
-		}
-		for (mpz_set_ui(j,1); mpz_cmp(j,s) < 0; mpz_add_ui(j, j, 1)){ //for (j= 1; j <= s-1; j++)
-				
-			mpz_mul(y, y, y); // y = y*y
-			mpz_mod(y, y, n); // y = (y*y) % n
-			if ( mpz_cmp_ui(y,1) == 0 ){ 
-				gmp_printf("Le nombre %Zd est composé \n \n",n); 
-				//mpz_clears(t, i, a, y, j, s, n_1); // Libération mémoire
-				return; // On arrête la fonction.
+		square_multiply(y, a, n, tmp_t);
+		mpz_set(tmp_t,t); // tmp_t = t car il a été mis à zéro par square_multiply
+		
+		if ( (mpz_cmp_ui(y, 1) != 0) && (mpz_cmp(y, n_1) != 0)){
+
+				for (mpz_set_ui(j,1); mpz_cmp(j,s) < 0; mpz_add_ui(j, j, 1)){ //for (j= 1; j <= s-1; j++)
+					
+				mpz_mul(y, y, y); // y = y*y
+				mpz_mod(y, y, n); // y = (y*y) % n
+				if ( mpz_cmp_ui(y,1) == 0 ){ 
+
+					// Libération mémoire
+					mpz_clear(t); mpz_clear(i); mpz_clear(a); mpz_clear(y); mpz_clear(j); mpz_clear(s);
+					mpz_clear(n_1);gmp_randclear(state);
+					return 0; // On arrête la fonction.
+				}
+				if ( mpz_cmp(y,n_1) == 0 ) {
+					next = 1;
+					break; // On sort de la boucle j et on continue i
+				}
 			}
-			if ( mpz_cmp_ui(y,-1) == 0 ) break; // On sort de la boucle j
+		if ( next == 1) break; // On continue 
+		else return 0;
 		}
 	}
-	gmp_printf("Le nombre %Zd est premier \n",n);
-	//mpz_clears(t, i, a, y, j, s, n_1); // Libération mémoire
+
+	// Libération mémoire
+	mpz_clear(t); mpz_clear(i); mpz_clear(a); mpz_clear(y); mpz_clear(j); mpz_clear(s);
+	mpz_clear(n_1);gmp_randclear(state);
+	return 1;
 	}
+}
+
+
+void square_multiply(mpz_t r, mpz_t a, mpz_t n, mpz_t h){ // r : resultat de la fonction, n : modulo, h : exposant 
+	mpz_t reste;	// "reste" va contenir le reste de la division euclidienne de h par 2 à chaque itération.	
+	mpz_init(reste);
+	mpz_set(r,a);	//r = a
+
+	int i = 0;
+	int* tab = malloc(sizeof(int)*1024); // allocation mémoire d'un tableau de taille 1024
+	// Converstion de h en binaire
+	while( mpz_cmp_ui(h,0) > 0 ){
+		mpz_fdiv_qr_ui(h, reste, h, 2); // reste = reste de la div et h = h/2 pour la prochaine itération
+		if(mpz_cmp_ui(reste,0) == 0){
+			tab[i] = 0;
+		}else{
+			tab[i] = 1;
+		}
+		i++;
+	}
+	i--; // on ne prend pas en compte t
+
+	while (i > 0){ 	//mpz_cmp_ui(h, 1) > 0){
+		mpz_mul(r, r, r); // r = r*r
+		mpz_mod(r, r, n); // r = r*r modulo n
+		if ( tab[--i] == 1 ){ // Bit à 1
+			mpz_mul(r, r, a); // r = r*a
+			mpz_mod(r, r, n); // r = r*a modulo n
+		}
+	}
+	
+	// Libération mémoire
+	free(tab);
+	tab = NULL;
+	mpz_clear(reste);
 }
 
 int main(){ 
 	
-	mpz_t t, s, n, k;
-	mpz_init_set_str(n,"13",10); 
-	mpz_init_set_str(k,"5",10); 
-	mpz_inits(t, s,(void*)0);
+	int res = 0;
+	char* str;
+	mpz_t mod, exp, n, a;
+	mpz_init(n);
+	mpz_init_set_ui(a,427); 
+	mpz_init_set_str(mod,"300",10); 
+	mpz_init_set_str(exp,"12",10); 	
+
+	mpz_set_ui(n,13);
+	// Test convertisseur binaire:
+	gmp_printf("%Zd : %s\n", n, str = mpz_get_str(NULL, 2, n));
+	printf("taille de n : %d\n", (int)strlen(str));
 
 	// Test de la fonction de Fermat:
+	mpz_set_ui(n,4);
 	printf("\n### TEST DE FERMAT ###\n");	
-	testFermat(n,k);
+	res = testFermat(n,mod);
+	if ( res == 1) gmp_printf("Le nombre %Zd est premier \n \n",n);
+	else gmp_printf("Le nombre %Zd est composé \n \n",n);
 
 	// Test de la fonction de miller rabin:
 	printf("\n### TEST DE MILLER RABIN ###\n");
-	testMillerRabin(n,k);
-	
+	res = testMillerRabin(n,mod);
+	if ( res == 1) gmp_printf("Le nombre %Zd est premier \n \n",n);
+	else gmp_printf("Le nombre %Zd est composé \n \n",n);
 	
 
 	return 0;
 }
+
